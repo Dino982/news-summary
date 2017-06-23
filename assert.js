1afa639db704c184f555c3bd61ea6a70ab018fe8
Andrew Clarke <andrewclarke982@gmail.com>
2017-06-23 11:21:03 +0100

    Initial commit

---
+++ assert.js
@@ -0,0 +1,9 @@
+var assert = {
+  isTrue: function(assertionToCheck) {
+    if (!assertion){
+      throw new Error('Fail :(');
+    } else {
+      console.log('Code looks good!');
+    };  
+  };
+};
